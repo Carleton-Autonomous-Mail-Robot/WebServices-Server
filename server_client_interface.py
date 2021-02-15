2581956e2b9d060cdd3a3576c4d741b51854331f
Gabriel Ciolac <gabrielciolac@gmail.com>
2021-02-15 18:42:28 -0500

    Removed hard debugging

--- server_client_interface.py
+++ server_client_interface.py
@@ -6,35 +6,27 @@ import json
 app = Flask(__name__)
 CORS(app)
 
-clients = {}
+clients = dict()
 
 
 @app.route('/',methods=['POST'])
 def inbox():
-	
-	jsonrec = json.loads(request.data.decode('utf-8'))
-	
-	fileDebug = open("debug.txt", "w")
-	fileDebug.write(str(jsonrec))
-	fileDebug.close()
-	
-	if jsonrec is None:
-		return jsonify(status='bad')
-
 	# Identify and process request
-	if jsonrec['opperation'] == 'getMessage':
-		return __get_message(jsonrec['clientID'])
-	elif jsonrec['opperation'] == 'leaveMessage':
-		return __leave_message(jsonrec['clientID'],jsonrec['payload'])
-	elif jsonrec['opperation'] == 'newClient':
-		return __new_client(None, jsonrec["payload"]["pickup"])
-	elif jsonrec['opperation'] == 'delete':
-		return __delete(jsonrec["clientID"])
+	if request.json['opperation'] == 'getMessage':
+		return __get_message(request.json['clientID'])
+	elif request.json['opperation'] == 'leaveMessage':
+		return __leave_message(request.json['clientID'],request.json['payload'])
+	elif request.json['opperation'] == 'newClient':
+		return __new_client(request.json["payload"]["type"])
+	elif request.json['opperation'] == 'delete':
+		return __delete(request.json["clientID"])
+	else:
+		return __returnResponse() #returns a bad request
 		
 	
 
 
-def __new_client(cID, pickup):
+def __new_client():
 	if (pickup != "") :		# if there is a listed pickup, its a request from a user
 		robotID = ""
 		for x in clients :
@@ -48,33 +40,39 @@ def __new_client(cID, pickup):
 		
 		clients[cID] = Client(cID)
 		
-		response = jsonify(status = "done", clientID = robotID, payload = {"sender": cID})
-		response.headers["Access-Control-Allow-Origin"] = "*"
-		response.headers["Access-Control-Allow-Credentials"] = True
-		response.headers["Access-Control-Allow-Methods"] = "POST"
-		response.headers["Content-Type"] = "application/json"
-		
 		return response
 	else :		# its a robot
 		clients[cID] = Client(cID)
-		return jsonify(status = "done", clientID = cID)
+		return __returnResponse(status = "done", clientID = cID)
 
 
 def __leave_message(clientID,msg):
 	clients[clientID].leave_message(msg)
-	return jsonify(status='done')
+	return __returnResponse(status='done')
 
 
 def __get_message(clientID):
 	try:
-		return jsonify(status='done',payload=clients[clientID].next_message())
+		return __returnResponse(status='done',payload=clients[clientID].next_message())
 	except:
-		return jsonify(status='bad')
+		return __returnResponse(status='bad')
 
 
 def __delete(clientID) :
 	try:
 		del clients[clientID]
-		return jsonify(status = "done")
+		return __returnResponse(status = "done")
 	except:
-		return jsonify(status = "bad")
+		return __returnResponse(status = "bad")
+
+def __returnResponse(status='bad',clientID = None, payload=None):
+	response = jsonify(status = "done", clientID = robotID, payload = {"sender": cID})
+	response.headers["Access-Control-Allow-Origin"] = "*"
+	response.headers["Access-Control-Allow-Credentials"] = True
+	response.headers["Access-Control-Allow-Methods"] = "POST"
+	response.headers["Content-Type"] = "application/json"
+	
+	if not (clientID is None):
+		response.set_cookie('clientID',clientID)
+
+	return response
