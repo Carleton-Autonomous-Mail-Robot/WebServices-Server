c58bc72962bdd0bb34e7a5cf99c78493003bc632
gabrielciolac <gabrielciolac@gmail.com>
2021-02-20 11:37:56 -0500

    updated scheduler for drive command

--- app/scheduler.py
+++ app/scheduler.py
@@ -14,30 +14,35 @@ class Scheduler():
             self.__robots.append(client_id)
             return
         self.__others.append(client_id)
+
+    def get_robot_id(self)->int:
+        for robot in self.__robots:
+            if not self.__controller[robot].has_mail():
+                return robot
+        return 0
+    
+
+    def __other_msg_controller(self,client_id,msg)->bool:
+        if 'drive' in msg:
+            self.__controller.leaveMail(self.get_robot_id, '1,0')
+            return True
+        return False
+        
+
+    def __robot_msg_controller(self,client_id,msg)->bool:
+        return False
     
     '''
         will decide what todo with the message, and deposit the result in the appropriate mailbox
     '''
     def message_handler(self,client_id,msg)->bool:
+        if client_id in self.__others:
+            return self.__other_msg_controller(client_id,msg)
+        elif client_id in self.__robots:
+            return self.__robot_msg_controller(client_id,msg)
+        return False
+            
 
-        try:
-            # If message requesting robot service
-            if msg["currentLocation"] == "":
-                for robot in self.__robots:
-                    if not self.__controller.has_mail(robot):
-                        self.__controller.leaveMail(robot,msg)
-                        return True
-                # No available robots
-                return False
-                
-            # If message contains location information    
-            else:
-                if self.__controller.exists(client_id):
-                    self.__controller.leaveMail(client_id,msg)
-                    return True
-                
-        except:
-            return True
             
     def delete(self, cid)->bool:
         x = 0
